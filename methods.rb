a5d219a5d0e5908f4ae24d917a62789d764ff731
Whitney Hodgkins <hodgkins.whitney@gmail.com>
2020-03-16 01:43:23 +0000

    Automatically backed up by Learn

---
+++ methods.rb
@@ -0,0 +1,8 @@
+# method name first_parameter, second_parameter
+def greeting_programmer(name, language)
+  puts "Hello, #{name}. We heard you are a great #{language} programmer."
+end
+
+greeting_programmer("Maria", "Ruby")
+
+greeting_programmer("Steven, "Elixer") 
